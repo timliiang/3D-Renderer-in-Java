0dc6db201c5384a05200c1a54934a836fd646a01
M0chiiii <90522107+M0chiiii@users.noreply.github.com>
2025-07-11 20:29:33 -0400

    one sphere shows now

--- src/main/java/io/github/timliiang/scene/Render.java
+++ src/main/java/io/github/timliiang/scene/Render.java
@@ -21,8 +21,8 @@ public class Render {
 
         Sphere[] spheres = {
             new Sphere(
-                new Vec3(0f, 0f, -1.2f), 
-                0.4f,
+                new Vec3(0f, 0f, 20f), 
+                10f,
                 new Material(
                     new Color(0.204f, 0.1f, 0f),
                     new Color(0.8f, 0.4f, 0f),
@@ -30,19 +30,19 @@ public class Render {
                     80
             )),
             new Sphere(
-                new Vec3(-1f, 0f, -0.8f), 
-                0.2f,
+                new Vec3(25f, 0f, 30f), 
+                2f,
                 new Material(
-                    new Color(0f, 0f, 0f),
+                    new Color(0.07f, 0.07f, 0.07f),
                     new Color(0f, 0f, 1f),
                     new Color(1f, 1f, 1f),
                     25
             )),
             new Sphere(
-                new Vec3(1f, 0f, -0.8f), 
-                0.2f,
+                new Vec3(-50f, 20f, 100f), 
+                30,
                 new Material(
-                    new Color(0f, 0f, 0f),
+                    new Color(0.07f, 0.07f, 0.07f),
                     new Color(0f, 1f, 0f),
                     new Color(1f, 1f, 1f),
                     50
@@ -52,24 +52,26 @@ public class Render {
         Light[] lights = {
             new Light(
                 new Vec3(20f, 30f, 30f),
-                new Color(1f, 0.7f, 1f),
-                new Color(1f, 1f,1f)),
+                new Color(0.5f, 0.4f, 1f),    // warm orange/yellow diffuse
+                new Color(1f, 0.95f, 0.8f)    // warm specular
+            ),
             new Light(
                 new Vec3(-55f, 30f, 90f),
-                new Color(1f, 1f, 0.6f),
-                new Color(1f, 1f,1f)
+                new Color(0.6f, 0.8f, 0.8f),   // cool blue diffuse
+                new Color(0.8f, 0.9f, 1f)    // cool specular
             )
         };
 
         
         // Screen
-        Vec3 x1 = new Vec3(1f, 0.75f, 0f); // + +
-        Vec3 x2 = new Vec3(-1f, 0.75f, 0f); // - +
-        Vec3 x3 = new Vec3(1f, -0.75f, 0f); // + -
-        Vec3 x4 = new Vec3(-1f, -0.75f, 0f); // - -
+        Vec3 x1 = new Vec3(1f, 0.75f, 0f);
+        Vec3 x2 = new Vec3(-1f, 0.75f, 0f);
+        Vec3 x3 = new Vec3(1f, -0.75f, 0f);
+        Vec3 x4 = new Vec3(-1f, -0.75f, 0f);
 
         Vec3 c = new Vec3(0f, 0f, -1f); 
 
+        
         for (int x = 0; x < WIDTH; x++) {
             for (int y = 0; y < HEIGHT; y++) {
                 float alpha = x / (float)(WIDTH - 1); 
@@ -97,13 +99,12 @@ public class Render {
                     smallestMat = smallestSphere.getMaterial();
                     Vec3 pSphere = Vec3.add(ray.getOrigin(), ray.getDirection().scale(smallestT));
                     Vec3 surfNorm = Vec3.sub(pSphere, smallestSphere.getCenter()).normalize();
-                    //color = Color.multiply(smallestMat.getAmbient(), ambient);
+                    color = Color.multiply(smallestMat.getAmbient(), ambient);
                     Color diffuseComp = null;
                     Color specComp = null;
                     for (int i = 0; i < lights.length; i++) {
                         Vec3 lightVec = Vec3.sub(lights[i].getLocation(), pSphere).normalize();
                         float temp = Vec3.dot(surfNorm, lightVec);
-                        System.out.println(temp);
                         if (temp < 0) continue;
                         diffuseComp = Color.multiply(smallestMat.getDiffuse(), lights[i].getDiffuse()).scale(temp);
                         Vec3 reflVec = Vec3.sub(surfNorm.scale(2 * temp), lightVec);
@@ -114,16 +115,13 @@ public class Render {
                                 (float) Math.pow(
                                     Vec3.dot(viewVec, reflVec), 
                                     smallestMat.getShininess()));
-                        //color = Color.add(color, diffuseComp);
-                        //color = Color.add(color, specComp);
+                        color = Color.add(color, diffuseComp);
+                        color = Color.add(color, specComp);
                         color.clamp();
                     }
-                    color = smallestMat.getAmbient();
+                    
                 }
-
-
-                image.setRGB(x, (HEIGHT - 1 - y), new Color((float) x / WIDTH, (float) y / HEIGHT, 100 / 255f).getRGB());
-                //image.setRGB(x, y, color.getRGB());
+                image.setRGB(x, y, color.getRGB());
             }
         }
 
