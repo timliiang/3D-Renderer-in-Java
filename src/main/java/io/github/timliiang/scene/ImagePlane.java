15d943d5250fb99fe13ddf8ef63365e2c0a5aa8e
M0chiiii <90522107+M0chiiii@users.noreply.github.com>
2025-07-17 00:22:28 -0400

    cleaned up code for proj3

--- src/main/java/io/github/timliiang/scene/ImagePlane.java
+++ src/main/java/io/github/timliiang/scene/ImagePlane.java
@@ -3,8 +3,32 @@ package io.github.timliiang.scene;
 import io.github.timliiang.math.Vec3;
 
 public class ImagePlane {
-    Vec3 x1;
-    Vec3 x2;
-    Vec3 x3;
-    Vec3 x4;
+    private Vec3 x1;
+    private Vec3 x2;
+    private Vec3 x3;
+    private Vec3 x4;
+
+    public ImagePlane(Vec3 x1, Vec3 x2, Vec3 x3, Vec3 x4) {
+        this.x1 = x1;
+        this.x2 = x2;
+        this.x3 = x3;
+        this.x4 = x4;
+    }
+    
+    public Vec3 getX1() {
+        return this.x1;
+    }
+
+    public Vec3 getX2() {
+        return this.x2;
+    }
+
+    public Vec3 getX3() {
+        return this.x3;
+    }
+
+    public Vec3 getX4() {
+        return this.x4;
+    }
+
 }
--- src/main/java/io/github/timliiang/scene/Render.java
+++ src/main/java/io/github/timliiang/scene/Render.java
@@ -13,79 +13,103 @@ import io.github.timliiang.math.*;
 public class Render {
     private static int WIDTH = App.WIDTH;
     private static int HEIGHT = App.HEIGHT;
+
+    private static final Color AMBIENT = new Color(0.5f, 0.5f, 0.5f); // i_a, ambient intensity
+    private static Vec3 camera = new Vec3(0f, 0f, -1f);
+
+    private static ImagePlane imagePlane = new ImagePlane(
+        new Vec3(1f, 0.75f, 0f),
+        new Vec3(-1f, 0.75f, 0f),
+        new Vec3(1f, -0.75f, 0f),
+        new Vec3(-1f, -0.75f, 0f)
+    );
+
+    private static Sphere[] spheres = {
+        new Sphere(
+            new Vec3(0f, 0f, 10f), 
+            2f,
+            new Material(
+                new Color(0.204f, 0.1f, 0f),
+                new Color(0.8f, 0.4f, 0f),
+                new Color(1f, 1f, 1f),
+                80
+        )),
+        new Sphere(
+            new Vec3(15f, 0f, 20f), 
+            3f,
+            new Material(
+                new Color(0.07f, 0.205f, 0.7f),
+                new Color(0.8f, 0.4f, 0f),
+                new Color(1f, 1f, 1f),
+                80
+        )),
+        new Sphere(
+            new Vec3(-12f, 6f, 20f), //-50, 20, 100
+            5f,
+            new Material(
+                new Color(0.07f, 0.8f, 0.07f),
+                new Color(0.8f, 0.4f, 0f),
+                new Color(1f, 1f, 1f),
+                50
+        ))
+    };
+
+    private static Light[] lights = {
+        new Light(
+            new Vec3(30f, 40f, 30f),
+            new Color(0.6f, 0.8f, 0.8f),    // warm orange/yellow diffuse
+            new Color(0.8f, 0.9f, 1f)    // warm specular
+        ),
+        new Light(
+            new Vec3(-100f, 60f, -30f),
+            new Color(0.6f, 0.8f, 0.8f),
+            new Color(0.8f, 0.9f, 1f)
+        )
+    };
     
+
+    /*
+     * 
+     */
     public static void render() {
         BufferedImage image = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_ARGB);
         Frame frame = new Frame(WIDTH, HEIGHT);
-        Color ambient = new Color(0.5f, 0.5f, 0.5f); // i_a, ambient intensity
 
-        Sphere[] spheres = {
-            new Sphere(
-                new Vec3(0f, 0f, 10f), 
-                2f,
-                new Material(
-                    new Color(0.204f, 0.1f, 0f),
-                    new Color(0.8f, 0.4f, 0f),
-                    new Color(1f, 1f, 1f),
-                    80
-            )),
-            new Sphere(
-                new Vec3(15f, 0f, 20f), 
-                3f,
-                new Material(
-                    new Color(0.07f, 0.205f, 0.7f),
-                    new Color(0.8f, 0.4f, 0f),
-                    new Color(1f, 1f, 1f),
-                    80
-            )),
-            new Sphere(
-                new Vec3(-12f, 6f, 20f), //-50, 20, 100
-                5f,
-                new Material(
-                    new Color(0.07f, 0.8f, 0.07f),
-                    new Color(0.8f, 0.4f, 0f),
-                    new Color(1f, 1f, 1f),
-                    50
-            ))
-        };
+        tracePixel(image);
 
-        Light[] lights = {
-            new Light(
-                new Vec3(30f, 40f, 30f),
-                new Color(0.6f, 0.8f, 0.8f),    // warm orange/yellow diffuse
-                new Color(0.8f, 0.9f, 1f)    // warm specular
-            ),
-            new Light(
-                new Vec3(-100f, 60f, -30f),
-                new Color(0.6f, 0.8f, 0.8f),
-                new Color(0.8f, 0.9f, 1f)
-            )
-        };
+        JLabel label = new JLabel(new ImageIcon(image));
+        frame.add(label);
+        frame.setVisible(true);
+    }
 
-        
-        // Screen
-        Vec3 x1 = new Vec3(1f, 0.75f, 0f);
-        Vec3 x2 = new Vec3(-1f, 0.75f, 0f);
-        Vec3 x3 = new Vec3(1f, -0.75f, 0f);
-        Vec3 x4 = new Vec3(-1f, -0.75f, 0f);
-
-        Vec3 c = new Vec3(0f, 0f, -1f); 
-        
+    /*
+     * This method traces through each pixel and determines the color based from the ray it casts
+     * 
+     */
+    public static void tracePixel(BufferedImage image) {
         for (int x = 0; x < WIDTH; x++) {
             for (int y = 0; y < HEIGHT; y++) {
                 float alpha = x / (float)(WIDTH - 1); 
                 float beta = y / (float)(HEIGHT - 1);
-                Vec3 t = Vec3.lerp(x1, x2, alpha);
-                Vec3 b = Vec3.lerp(x3, x4, alpha);
+                Vec3 t = Vec3.lerp(
+                    imagePlane.getX1(), 
+                    imagePlane.getX2(), 
+                    alpha
+                );
+                Vec3 b = Vec3.lerp(
+                    imagePlane.getX3(), 
+                    imagePlane.getX4(), 
+                    alpha
+                );
                 Vec3 p = Vec3.lerp(t, b, beta);
-                Ray ray = new Ray(p, p.sub(c));
 
-                Color color = new Color(0f, 0f, 0f); // pixel colour
+                Ray ray = new Ray(p, p.sub(camera));
+                Color color = new Color(0f, 0f, 0f);
 
                 Float smallestT = Float.NaN;
                 Sphere smallestSphere = null;
-                Material smallestMat = null;
 
+                // Find closest object the ray intersects if any
                 for (int s = 0; s < spheres.length; s++) {
                     Optional<Float> opt = spheres[s].intersection(ray);
                     if (opt.isPresent() && (smallestT.isNaN() || opt.get() < smallestT)) {
@@ -94,38 +118,58 @@ public class Render {
                     }
                 }
 
-                // proj 3 step 4
+                // Phong's Illumination Model
                 if (!smallestT.isNaN()) {
-                    smallestMat = smallestSphere.getMaterial();
-                    Vec3 pSphere = ray.getOrigin().add(ray.getDirection().scale(smallestT));
-                    Vec3 surfNorm = pSphere.sub(smallestSphere.getCenter()).normalize();
-                    color = Color.multiply(smallestMat.getAmbient(), ambient);
-                    Color diffuseComp = null;
-                    Color specComp = null;
-                    for (int i = 0; i < lights.length; i++) {
-                        Vec3 lightVec = lights[i].getLocation().sub(pSphere).normalize();
-                        float temp = surfNorm.dot(lightVec);
-                        if (temp < 0) continue;
-                        diffuseComp = Color.multiply(smallestMat.getDiffuse(), lights[i].getDiffuse()).scale(temp);
-                        Vec3 reflVec = surfNorm.scale(2 * temp).sub(lightVec);
-                        Vec3 viewVec = c.sub(pSphere).normalize();
-                        specComp = Color.multiply(smallestMat.getSpecular(), lights[i].getSpecular()).scale(
-                                (float) Math.pow(
-                                    viewVec.dot(reflVec), 
-                                    smallestMat.getShininess()));
-                        color = color.add(diffuseComp);
-                        color = color.add(specComp);
-                        color = color.clamp();
-                    }
-                    
+                    color = rayColor(smallestT, smallestSphere, ray);
                 }
+                
+                // Paint pixel
                 image.setRGB(x, y, color.getRGB());
             }
         }
+    }
 
-        JLabel label = new JLabel(new ImageIcon(image));
-        frame.add(label);
-        frame.setVisible(true);
+    /*
+     * This method determines the color of a ray by using Phong's Illumination Model.
+     *  The color consists of three components being Ambient, Diffuse, and Specular.
+     * 
+     */
+    public static Color rayColor(Float t, Sphere sphere, Ray ray) {
+        Material smallestMat = sphere.getMaterial();
+        Vec3 pSphere = ray.getOrigin().add(ray.getDirection().scale(t));
+        Vec3 surfNorm = pSphere.sub(sphere.getCenter()).normalize();
+
+        Color color = Color.multiply(smallestMat.getAmbient(), AMBIENT);
+        Color diffuseComp = null;
+        Color specComp = null;
+
+        for (int i = 0; i < lights.length; i++) {
+            Vec3 lightVec = lights[i].getLocation().sub(pSphere).normalize();
+            float temp = surfNorm.dot(lightVec);
+            if (temp < 0) continue;
+
+            // Calculate diffuse component
+            diffuseComp = Color.multiply(smallestMat.getDiffuse(), lights[i].getDiffuse()).scale(temp);
+            Vec3 reflVec = surfNorm.scale(2 * temp).sub(lightVec);
+            Vec3 viewVec = camera.sub(pSphere).normalize();
+
+            // Calculate specular component
+            specComp = Color.multiply(
+                smallestMat.getSpecular(), 
+                lights[i].getSpecular()
+            );
+            specComp = specComp.scale(
+                (float) Math.pow(
+                    viewVec.dot(reflVec), 
+                    smallestMat.getShininess()));
+
+            // Add the color components
+            color = color.add(diffuseComp);
+            color = color.add(specComp);
+            color = color.clamp();
+        }
+
+        return color;
     }
 
 }
